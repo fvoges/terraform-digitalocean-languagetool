a6150072a982645d03a403d5703f182b3e2d3f42
Federico Voges <fvoges@users.noreply.github.com>
2022-07-29 20:29:33 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,15 +30,14 @@ module "languagetool" {
   source  = "fvoges/ubuntu/digitalocean"
   version = "1.0.0"
 
-  application    = "languagetool"
-  autosign_token = var.autosign_token
-  dns_ttl        = 30
-  domain         = "voges.uk"
-  hostname       = "languagetool"
-  manage_project = false
-  # project_name   = digitalocean_project.languagetool.name
-  project_name   = "LanguageTool"
-  puppet_server  = "pe.shadowsun.com.ar"
-  tags           = [ "http", "https" ]
-  vpc_id         = data.digitalocean_vpc.default.id
+  puppet_application    = "languagetool"
+  puppet_autosign_token = var.autosign_token
+  puppet_server         = "pe.shadowsun.com.ar"
+  dns_ttl               = 30
+  domain                = "voges.uk"
+  hostname              = "languagetool"
+  manage_project        = true
+  project_name          = digitalocean_project.languagetool.name
+  tags                  = [ "http", "https" ]
+  vpc_id                = data.digitalocean_vpc.default.id
 }
